866e2b437ae2cb9b57d32f0c5ba196d6521f0791
sjpark538 <phi5388@gmail.com>
2022-08-20 16:45:04 -0400

    jobboard update

--- src/components/JobBoard/JobBoard.js
+++ src/components/JobBoard/JobBoard.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './JobBoard.scss';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import sortIcon from "../../assets/icons/sort-24px.svg"
+import { v4 as uuidv4 } from 'uuid';
+import rightArrow from '../../assets/icons/chevron_right-24px.svg';
 
 class JobBoard extends React.Component {
     state = {
         currentJobboard: [],
     };
 
-    
 
     componentDidMount() {
         axios.get(`http://localhost:8080/job`).then((res) => {
@@ -22,23 +25,65 @@ class JobBoard extends React.Component {
         return (
         <>
         <Header />
+        <div className='jobboard'>
+        <div>This is Job board</div>
+        <section className="job__horizontal">
+          <div className="job__horizontal-label">
+            <h5 className="job__horizontal-label--title">POST DATE
+            </h5>
+            <img className="job__sort" src={sortIcon} alt="sort-icon" />
+          </div>
+          <div className="job__horizontal-label">
+            <h5 className="job__horizontal-label--title">TITLE</h5>
+            <img className="job__sort" src={sortIcon} alt="sort-icon" />
+          </div>
+          <div className="job__horizontal-label">
+            <h5 className="job__horizontal-label--title">CATEGORY</h5>
+            <img className="job__sort" src={sortIcon} alt="sort-icon" />
+          </div>
+          <div className="job__horizontal-label">
+            <h5 className="job__horizontal-label--title">PAY TYPE</h5>
+            <img className="job__sort" src={sortIcon} alt="sort-icon" />
+          </div>
+          <div className="details__horizontal-label">
+            <h5 className="job__horizontal-label--title">DUE DATE</h5>
+          </div>
+        </section>
+        
         {this.state.currentJobboard.map((joblist) => (
-            <>
-            <div>THIS IS JOBBOARD COMPONENT</div>
-            <div>{joblist.title}</div>
-            </>
+            <div className="job__items" key={uuidv4()}>
+                <div className="job__items-detail">
+                    <div className="job__items-detail--list">
+                        <h5 className="job__items-title">POST DATE</h5>
+                        <p className='job__items-postdate'>{new Date(joblist.postDate).toLocaleDateString()}</p>
+                    </div>
+                    <div className="job__items-detail--list">
+                    <Link to ={`/job/${joblist.id}`}>
+                        <h5 className="job__items-title">TITLE</h5>
+                        <p className='job__items-title'>{joblist.title}</p>
+                        <img
+                        className="inventory__items-arrow"
+                        src={rightArrow}
+                        alt="sort-icon"
+                        />
+                    </Link>
+                    </div>
+                    <div className="job__items-detail--list">
+                        <h5 className="job__items-title">CATEGORY</h5>
+                        <p className='job__items-category'>{joblist.category}</p>
+                    </div>
+                    <div className="job__items-detail--list">
+                        <h5 className="job__items-title">PAY TYPE</h5>
+                        <p className='job__items-postdate'>{joblist.payType}</p>
+                    </div>
+                </div>
+            </div>
         ))}
+        </div>
+        <Footer />
         </>
-      );
+    );
     }
-
-    // render(){
-    //     return(
-    //       <>
-    //       <div>THIS IS JOBBOARD COMPONENT</div>
-    //       </>
-    //     )
-    //   }
   }
   
   export default JobBoard;
